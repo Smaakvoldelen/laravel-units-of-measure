5b120b309bcf39fa0f50a1d615c3d5beeb3d1dd3
Michael Beers <michael@michaelbeers.nl>
2024-07-16 09:26:39 +0200

    Fix temperature unit

--- src/Units/Temperature.php
+++ src/Units/Temperature.php
@@ -2,6 +2,12 @@
 
 namespace Smaakvoldelen\UnitsOfMeasure\Units;
 
+use Smaakvoldelen\UnitsOfMeasure\Measurements\TemperatureMeasurement;
 use Smaakvoldelen\UnitsOfMeasure\Unit;
 
-class Temperature extends Unit {}
+class Temperature extends Unit {
+    /**
+     * The class name where all the measurements are stored for the unit.
+     */
+    public static string $measurementClass = TemperatureMeasurement::class;
+}
